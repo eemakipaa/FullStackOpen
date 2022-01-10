86f822c336a090d88cc8e6d2fc8d6f8b95fc1729
Eero Mäkipää <eero.h.makipaa@gmail.com>
2022-01-10 09:04:09 +0200

    submit unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ * Full Stack open 2021 - Unicafe
+ * Author: Eero Mäkipää
+ * 
+ * Simple feedback giving application. User can give good, neutral or bad feedback.
+ * Application presents statistical data on given feedback.
+ */
+import React, {useState} from "react";
 
-function App() {
+// Components
+
+const Button = ({handleClick, text}) => {
+  return(<button onClick={handleClick} >{text}</button>)
+}
+ 
+const StatisticLine = ({text, value}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <tr>
+      <td>{text}: {value}</td>
+    </tr>
+  )
 }
 
-export default App;
+/*
+ * Component renders a table to show feedback given by user. If any feedback
+ * isn't given, component renders "No feedback given" line
+ */
+const Statistics = ({good, neutral, bad}) => {
+  const countAverage = () => {
+    let sum = good * 1 + bad * -1
+    return (sum / (good + neutral + bad))
+  }
+  if (good + neutral + bad === 0) {
+    return (
+      <div>
+        <h3>No feedback given</h3>
+      </div>
+    )
+  } 
+  else {
+    return (
+      <table>
+        <thead style={{textAlign: 'left'}}>
+          <tr>
+            <th>Statistics</th>
+          </tr>    
+        </thead>
+        <tbody>
+          <StatisticLine text={"good"} value={good} />
+          <StatisticLine text={"neutral"} value={neutral} />
+          <StatisticLine text={"bad"}  value={bad}/>
+          <StatisticLine text={"all"} value={good + neutral + bad} />
+          <StatisticLine text={"average"} value={countAverage()} />
+          <StatisticLine text={"positive"} value={good / (good + neutral + bad) * 100 + ' %'} />
+        </tbody>
+      </table>
+    )
+  } 
+}
+
+// App component
+const App = () => {
+  // State hooks
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+      <div>
+          <h1>Give feedback</h1>
+          <Button text="Good" handleClick={() => setGood(good + 1)} />
+          <Button text="Neutral" handleClick={() => setNeutral(neutral + 1)} />
+          <Button text="Bad" handleClick={() => setBad(bad + 1)} />
+          <Statistics good={good} neutral={neutral} bad={bad} />
+      </div>
+  )
+}
+
+export default App
